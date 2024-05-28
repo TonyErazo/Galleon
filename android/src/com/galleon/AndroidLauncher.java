3f8d2d2c2e86b4cb7112809bec021b5ba4c66e1e
Tony <teraz001@fiu.edu>
2024-05-28 02:25:38 -0500

    Initial Project Build

---
+++ android/src/com/galleon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.galleon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.galleon.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
